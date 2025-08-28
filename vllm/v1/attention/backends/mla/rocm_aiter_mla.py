fcd71afe32e957721cffce6ea454ae4eb518a1c1
ShaoChunLee <Shao-Chun.Lee@amd.com>
2025-08-28 16:25:39 +0000

    fix rope cache quant bug

--- vllm/v1/attention/backends/mla/rocm_aiter_mla.py
+++ vllm/v1/attention/backends/mla/rocm_aiter_mla.py
@@ -303,9 +303,18 @@ class AiterMLAImpl(MLACommonImpl[AiterMLAMetadata]):
         ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
         assert kv_c_and_k_pe_cache.numel() > 0
         assert attn_metadata.decode is not None
-
-        if envs.VLLM_AITER_TRITON_FUSED_ROPE_CACHE_CONCAT and q_nope_pe is not None and q_nope_zeros is not None:
-            q, o = q_nope_pe, q_nope_zeros
+        
+        if envs.VLLM_AITER_TRITON_FUSED_ROPE_CACHE_CONCAT and q_nope_pe is not None:
+            if q_nope_zeros is not None:
+                q, o = q_nope_pe, q_nope_zeros
+            else:
+                q = q_nope_pe
+                B = q.shape[0]
+                o = torch.empty(B,
+                                self.num_heads,
+                                self.kv_lora_rank,
+                                dtype=torch.bfloat16,
+                                device=q.device)
         else:
             if type(q) is tuple:
                 q = torch.cat(q, dim=-1)
