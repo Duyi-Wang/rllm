0617f219b953b52ecf1714ed7360e8169aee54f2
Duyi-Wang <duyi.wang@amd.com>
2025-10-15 15:45:04 +0000

    support cuda graph w/ mtp not for draft model

--- vllm/v1/attention/backends/mla/rocm_aiter_mla.py
+++ vllm/v1/attention/backends/mla/rocm_aiter_mla.py
@@ -80,7 +80,7 @@ class AiterMLAMetadataBuilder(MLACommonMetadataBuilder[AiterMLAMetadata]):
     # TODO(luka, lucas): audit this as part of:
     #  https://github.com/vllm-project/vllm/issues/22945
     cudagraph_support: ClassVar[AttentionCGSupport] = \
-        AttentionCGSupport.UNIFORM_SINGLE_TOKEN_DECODE
+        AttentionCGSupport.UNIFORM_BATCH
 
     def __init__(self, kv_cache_spec: AttentionSpec, layer_names: list[str],
                  vllm_config: VllmConfig, device: torch.device):
