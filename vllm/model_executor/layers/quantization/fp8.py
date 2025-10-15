844b320f39efef48b371582dc08c4a8e2cb1030a
billishyahao <yahao.he@gmail.com>
2025-10-15 13:47:55 +0800

    Revert "add aiter shared experts fusion"

--- vllm/model_executor/layers/quantization/fp8.py
+++ vllm/model_executor/layers/quantization/fp8.py
@@ -990,7 +990,6 @@ class Fp8MoEMethod(FusedMoEMethodBase):
             global_num_experts=global_num_experts,
             zero_expert_num=zero_expert_num,
             zero_expert_type=zero_expert_type,
-            num_fused_shared_experts=layer.num_fused_shared_experts,
         )
 
         #
