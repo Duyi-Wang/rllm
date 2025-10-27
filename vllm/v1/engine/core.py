ed3bf4a688524dd0cc6a43aec6356412180a9482
Duyi-Wang <duyi.wang@amd.com>
2025-10-27 22:08:24 +0800

    1

--- vllm/v1/engine/core.py
+++ vllm/v1/engine/core.py
@@ -728,6 +728,7 @@ class EngineCoreProc(EngineCore):
             self._process_input_queue()
             # 2) Step the engine core and return the outputs.
             self._process_engine_step()
+            dump_info_print()
 
     def _process_input_queue(self):
         """Exits when an engine step needs to be performed."""
